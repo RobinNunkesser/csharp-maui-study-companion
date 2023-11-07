e15492b8c3c7325023943be939b0d69d32e3700d
nunkesser <robin.nunkesser@hshl.de>
2023-11-07 12:01:57 +0100

    Exclude holidays on calendar export

--- StudyCompanion/Tabs/Courses/CoursesViewModel.cs
+++ StudyCompanion/Tabs/Courses/CoursesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using Italbytz.Ports.Meal;
@@ -11,6 +12,36 @@ namespace StudyCompanion
 {
     public class CoursesViewModel
     {
+        private static readonly string shortFormat = "dd.MM.yyyy";
+
+        public static DateTime SemesterEnd { get; } = DateTime.ParseExact("21.01.2024", shortFormat, CultureInfo.InvariantCulture);
+
+        public static List<DateTime> Holidays { get; } = new List<DateTime>
+        {
+            DateTime.ParseExact("03.10.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("01.11.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("24.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("25.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("26.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("27.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("28.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("29.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("30.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("31.12.2023",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("01.01.2024",shortFormat, CultureInfo.InvariantCulture),
+            /*DateTime.ParseExact("07.04.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("10.04.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("11.04.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("12.04.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("13.04.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("14.04.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("01.05.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("18.05.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("29.05.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("30.05.2024",shortFormat, CultureInfo.InvariantCulture),
+            DateTime.ParseExact("08.06.2024",shortFormat, CultureInfo.InvariantCulture)*/
+        };
+
         private readonly List<ICourse> courses;
         public SectionViewModel<ICourse>[]? GroupedCourses { get; set; }
 
@@ -30,7 +61,7 @@ namespace StudyCompanion
 
         private void SetGroupedCourses(List<ICourse> courses)
         {
-            SectionViewModel<ICourse>[] groups = new SectionViewModel<ICourse>[6];
+            SectionViewModel<ICourse>[] groups = new SectionViewModel<ICourse>[7];
             foreach (var group in courses.GroupBy(c => c.Semester))
             {
                 var section = new SectionViewModel<ICourse>()
@@ -62,18 +93,34 @@ namespace StudyCompanion
         }
 
 
-        internal async void AddCourseToCalendar(ICourse MockCourse, Calendar selectedCalendar)
+        internal async void AddCourseToCalendar(ICourse course, Plugin.Calendars.Abstractions.Calendar selectedCalendar)
         {
-            var startDate = DateTime.ParseExact(MockCourse.StartDate, MockCourse.DateFormat, System.Globalization.CultureInfo.InvariantCulture);
-            var endDate = startDate.AddMinutes(MockCourse.Length);
+            var startDate = DateTime.ParseExact(course.StartDate, course.DateFormat, System.Globalization.CultureInfo.InvariantCulture);
+            var endDate = startDate.AddMinutes(course.Length);
 
-            await CrossCalendars.Current.AddOrUpdateEventAsync(selectedCalendar, new CalendarEvent
+            while (startDate < SemesterEnd)
             {
-                Name = MockCourse.Name,
-                Start = startDate,
-                End = endDate,
-                Location = MockCourse.Room
-            });
+                var isHoliday = false;
+                foreach (var holiday in Holidays)
+                {
+                    if (startDate.Date == holiday.Date)
+                    {
+                        isHoliday = true;
+                        break;
+                    }
+                }
+
+                if (!isHoliday) await CrossCalendars.Current.AddOrUpdateEventAsync(selectedCalendar, new CalendarEvent
+                {
+                    Name = course.Name,
+                    Start = startDate,
+                    End = endDate,
+                    Location = course.Room
+                });
+
+                startDate = startDate.AddDays(course.Biweekly ? 14 : 7);
+                endDate = endDate.AddDays(course.Biweekly ? 14 : 7);
+            }
 
         }
 
